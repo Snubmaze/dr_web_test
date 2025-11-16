0f5bdfc68e27d1a930d0ac44cd0c1bfe68c7b6f3
Pavel Polyarus <keytohockey@gmail.com>
2025-11-16 15:31:40 +0300

    Final fixes

--- app/core/config.py
+++ app/core/config.py
@@ -2,15 +2,18 @@ import os
 
 
 class Settings:
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
+    def __init__(self):
+        self.POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+        self.POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
+        self.POSTGRES_DB = os.getenv("POSTGRES_DB", "task_queue")
+        self.POSTGRES_HOST = os.getenv("POSTGRES_HOST", "db")
+        self.POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
     
     @property
     def database_url(self) -> str:
         return (
             f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
-            f"@localhost:5435/{self.POSTGRES_DB}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
         )
 
 
